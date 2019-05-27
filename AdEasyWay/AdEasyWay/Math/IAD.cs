bc81f68b646a5068b17878084fb71c0d94d0fffc
ss <ss>
2019-05-27 21:32:34 +0900

    add interface for AD

---
+++ AdEasyWay/AdEasyWay/Math/IAD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdEasyWay.Math
+{
+    public interface IAD
+    {
+        double Value { get; }
+
+        double DerivedBy(IAD other);
+
+        IAD Subtract(IAD other);
+
+        IAD Multiply(IAD other);
+    }
+}
