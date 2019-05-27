bfaae22d995553e0b9497deac6e4958d23c564e0
ss <ss>
2019-05-27 22:37:57 +0900

    impl topdown

--- AdEasyWay/AdEasyWay/Math/TopDownAD.cs
+++ AdEasyWay/AdEasyWay/Math/TopDownAD.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MathNet.Numerics.Distributions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +15,7 @@ namespace AdEasyWay.Math
 
         public double DerivedBy(IAD other)
         {
-            if(other == this)
+            if (other == this)
             {
                 return 1;
             }
@@ -38,7 +39,6 @@ namespace AdEasyWay.Math
                     Tuple.Create( 1d, (IAD)this),
                     Tuple.Create( -1d, other),
                 }
-
             };
         }
 
@@ -55,9 +55,124 @@ namespace AdEasyWay.Math
                     Tuple.Create( r, (IAD)this),
                     Tuple.Create( l, other),
                 }
-
             };
         }
 
+        public IAD Add(IAD other)
+        {
+            var l = Value;
+            var r = other.Value;
+
+            return new TopDownAD
+            {
+                Value = l + r,
+                polynomial = new List<Tuple<double, IAD>>
+                {
+                    Tuple.Create( 1d, (IAD)this),
+                    Tuple.Create( 1d, other),
+                }
+            };
+        }
+
+        public IAD Divide(IAD other)
+        {
+            var l = Value;
+            var r = other.Value;
+
+            return new TopDownAD
+            {
+                Value = l / r,
+                polynomial = new List<Tuple<double, IAD>>
+                {
+                    Tuple.Create( 1/r, (IAD)this),
+                    Tuple.Create( -l / (r * r), other),
+                }
+            };
+        }
+
+        public IAD Divide(double other)
+        {
+            var l = Value;
+            var r = other;
+
+            return new TopDownAD
+            {
+                Value = l / r,
+                polynomial = new List<Tuple<double, IAD>>
+                {
+                    Tuple.Create( 1/r, (IAD)this),
+                }
+            };
+        }
+
+        public IAD Negative()
+        {
+            var l = Value;
+
+            return new TopDownAD
+            {
+                Value = -l,
+                polynomial = new List<Tuple<double, IAD>>
+                {
+                    Tuple.Create( -1d, (IAD)this),
+                }
+            };
+        }
+
+        public IAD Exp()
+        {
+            var l = Value;
+
+            return new TopDownAD
+            {
+                Value = System.Math.Exp(l),
+                polynomial = new List<Tuple<double, IAD>>
+                {
+                    Tuple.Create(System.Math.Exp(l), (IAD)this),
+                }
+            };
+        }
+
+        public IAD Log()
+        {
+            var l = Value;
+
+            return new TopDownAD
+            {
+                Value = System.Math.Log(l),
+                polynomial = new List<Tuple<double, IAD>>
+                {
+                    Tuple.Create(1/l, (IAD)this),
+                }
+            };
+        }
+
+        public IAD Sqrt()
+        {
+            var l = Value;
+
+            return new TopDownAD
+            {
+                Value = System.Math.Sqrt(l),
+                polynomial = new List<Tuple<double, IAD>>
+                {
+                    Tuple.Create(0.5 / System.Math.Sqrt(l), (IAD)this),
+                }
+            };
+        }
+
+        public IAD Cdf()
+        {
+            var l = Value;
+
+            return new TopDownAD
+            {
+                Value = Normal.CDF(0, 1, l),
+                polynomial = new List<Tuple<double, IAD>>
+                {
+                    Tuple.Create(Normal.PDF(0, 1, l), (IAD)this),
+                }
+            };
+        }
     }
 }
