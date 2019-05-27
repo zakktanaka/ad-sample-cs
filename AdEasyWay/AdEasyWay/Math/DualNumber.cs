b0507edf86f89e5c6e8ea045ed4ecee2f3b37d63
ss <ss>
2019-05-27 22:32:44 +0900

    impl dual number

--- AdEasyWay/AdEasyWay/Math/DualNumber.cs
+++ AdEasyWay/AdEasyWay/Math/DualNumber.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MathNet.Numerics.Distributions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -97,11 +98,144 @@ namespace AdEasyWay.Math
 
             var diffs = vals
                 .Select(v => new { Value = v, Differential = r * DerivedBy(v) + l * other.DerivedBy(v) })
-                .ToDictionary(pair =>pair.Value, pair => pair.Differential);
+                .ToDictionary(pair => pair.Value, pair => pair.Differential);
 
             return new DualNumber
             {
                 Value = l * r,
+                differentials = diffs,
+            };
+        }
+
+        public IAD Add(IAD other)
+        {
+            return Add(other, GetKeysWith(other));
+        }
+
+        private DualNumber Add(IAD other, IEnumerable<IAD> vals)
+        {
+            var l = Value;
+            var r = other.Value;
+
+            var diffs = vals
+                .Select(v => new { Value = v, Differential = DerivedBy(v) + other.DerivedBy(v) })
+                .ToDictionary(pair => pair.Value, pair => pair.Differential);
+
+            return new DualNumber
+            {
+                Value = l + r,
+                differentials = diffs,
+            };
+        }
+
+        public IAD Divide(IAD other)
+        {
+            return Divide(other, GetKeysWith(other));
+        }
+
+        private DualNumber Divide(IAD other, IEnumerable<IAD> vals)
+        {
+            var l = Value;
+            var r = other.Value;
+
+            var diffs = vals
+                .Select(v => new { Value = v, Differential = 1 / r * DerivedBy(v) - l / (r * r) * other.DerivedBy(v) })
+                .ToDictionary(pair => pair.Value, pair => pair.Differential);
+
+            return new DualNumber
+            {
+                Value = l / r,
+                differentials = diffs,
+            };
+        }
+
+        public IAD Divide(double other)
+        {
+            var l = Value;
+            var r = other;
+
+            var diffs = differentials.Keys
+                .Select(v => new { Value = v, Differential = 1 / r * DerivedBy(v) })
+                .ToDictionary(pair => pair.Value, pair => pair.Differential);
+
+            return new DualNumber
+            {
+                Value = l / r,
+                differentials = diffs,
+            };
+        }
+
+        public IAD Negative()
+        {
+            var l = Value;
+
+            var diffs = differentials.Keys
+                .Select(v => new { Value = v, Differential = -DerivedBy(v) })
+                .ToDictionary(pair => pair.Value, pair => pair.Differential);
+
+            return new DualNumber
+            {
+                Value = -l,
+                differentials = diffs,
+            };
+        }
+
+        public IAD Exp()
+        {
+            var l = Value;
+
+            var diffs = differentials.Keys
+                .Select(v => new { Value = v, Differential = System.Math.Exp(Value) * DerivedBy(v) })
+                .ToDictionary(pair => pair.Value, pair => pair.Differential);
+
+            return new DualNumber
+            {
+                Value = System.Math.Exp(Value),
+                differentials = diffs,
+            };
+        }
+
+        public IAD Log()
+        {
+            var l = Value;
+
+            var diffs = differentials.Keys
+                .Select(v => new { Value = v, Differential = 1 / l * DerivedBy(v) })
+                .ToDictionary(pair => pair.Value, pair => pair.Differential);
+
+            return new DualNumber
+            {
+                Value = System.Math.Log(Value),
+                differentials = diffs,
+            };
+        }
+
+        public IAD Sqrt()
+        {
+            var l = Value;
+
+            var diffs = differentials.Keys
+                .Select(v => new { Value = v, Differential = System.Math.Sqrt(l) * DerivedBy(v) })
+                .ToDictionary(pair => pair.Value, pair => pair.Differential);
+
+            return new DualNumber
+            {
+                Value = 0.5 / System.Math.Sqrt(l),
+                differentials = diffs,
+            };
+        }
+
+        public IAD Cdf()
+        {
+            var l = Value;
+
+            var diffs = differentials.Keys
+                .Select(v => new { Value = v, Differential = Normal.CDF(0, 1, l) * DerivedBy(v) })
+                .ToDictionary(pair => pair.Value, pair => pair.Differential);
+
+            return new DualNumber
+            {
+                Value = Normal.PDF(0, 1, l),
                 differentials = diffs,
             };
         }
