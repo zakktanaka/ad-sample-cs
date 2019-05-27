e8ee0e5caa758fd26783c3b0054a91720a201b87
ss <ss>
2019-05-27 21:07:58 +0900

    base

---
+++ AdEasyWay/AdEasyWay/Math/DualNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdEasyWay.Math
+{
+    public class DualNumber
+    {
+        public double Value { get; set; }
+
+        public DualNumber Multiply(DualNumber other)
+        {
+            return new DualNumber { Value = Value * other.Value };
+        }
+
+        public DualNumber Subtract(DualNumber other)
+        {
+            return new DualNumber { Value = Value - other.Value };
+        }
+    }
+}
--- AdEasyWay/AdEasyWay/Program.cs
+++ AdEasyWay/AdEasyWay/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdEasyWay.Math;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,25 @@ namespace AdEasyWay
 {
     class Program
     {
+        private static DualNumber Function(DualNumber x, DualNumber y)
+        {
+            return x.Multiply(x).Subtract(y.Multiply(y));
+        }
+
+        private static void TestFunction()
+        {
+            var x = new DualNumber { Value = 1 };
+            var y = new DualNumber { Value = 2 };
+
+            var ans = Function(x, y);
+
+            Console.WriteLine(ans.Value == (1 * 1 - 2 * 2));
+        }
+
         static void Main(string[] args)
         {
+            TestFunction();
+            Console.ReadKey();
         }
     }
 }
